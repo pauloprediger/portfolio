fe209836e6f43c3cdfde96af6e17c05e34d86d2a
Paulo Victor Ribeiro Prediger <142063372+pauloprediger@users.noreply.github.com>
2024-11-24 20:31:58 -0300

    Update index.jsx

--- src/pages/aboutMe/index.jsx
+++ src/pages/aboutMe/index.jsx
@@ -11,39 +11,53 @@ export default function AboutMe() {
         Olá eu sou o Paulo Prediger
         <img src={fotoSobreMim} alt="Foto minha" className={styles.fotoSobreMim} />
         <p className={styles.paragrafo}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores odio a beatae optio vitae
-          et blanditiis iure voluptatum animi, rerum voluptatem deleniti ad, iste fugiat voluptas,
-          molestiae possimus quod? Id.
+          Brasileiro nascido em 1999 no município de Quinze de Novembro, cidade interiorana do
+          centro norte do Estado do Rio Grande do Sul. Descendente de alemães, portugueses e
+          indígenas desde jovem tive contato com a terra, cresci em uma região agrícola e pacata,
+          muito próxima à natureza. Filho de cozinheira e comerciante, sempre estive envolvido com
+          processos relacionados à alimentação, desde o plantio até a etapa final de entrega e
+          experiência do consumidor. Quando jovem, por diversas vezes auxilieino plantio, cuidado e
+          coleta de alimentos cultivados pela família. Também em sua juventude contribui para com o
+          ramo alimentício familiar, onde cumpria diversas pequenas funções essenciais ao
+          funcionamento e manutenção do estabelecimento. Foi assim, trabalhando com a comida desde
+          cedo, que Paulo despertou interesse pela gastronomia, uma de suas grandes paixões.
         </p>
         <p className={styles.paragrafo}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat ipsam delectus nemo,
-          quasi quos iure aliquid voluptatum impedit aspernatur molestiae, provident, fugiat maxime
-          deserunt repellendus ex! Eius esse molestias maiores!
+          No período transitório da infância para adolescência surgiram novos interesses que viriam
+          a se tornar minha segunda paixão, a área da tecnologia. Foi mexendo em computadores e com
+          jogos eletrônicos que viria a ficar aficionado por informática e tecnologia, a ponto de
+          desejar construir uma carreira na área de desenvolvimento.
         </p>
         <p className={styles.paragrafo}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed debitis blanditiis enim
-          numquam harum porro modi. Architecto maxime maiores incidunt soluta ipsa magnam. In,
-          nesciunt a quasi temporibus sint architecto?
+          Estudei em escola pública durante toda a infância. Na adolescência, prestei processo
+          seletivo, no qual viria a ser aprovado, para me profissionalizar com um ensino médio
+          técnico. Foi então que, no ano de 2013, ingressei no Instituto Federal campus Ibirubá.
+          Durante esse período, estudei muitas das competências e habilidades essenciais para um
+          profissional de TI. Tais como: Algoritmos, lógica computacional, banco de dados,
+          programação, hardware, sistemas operacionais e redes de computadores. Vale citar que,
+          durante esse período, o fui selecionado como bolsista CNPq para desenvolvimento de um
+          aplicativo mobile para dispositivos Android. E como atividade extracurricular, no ano de
+          2015, participei do campeonato de programação que acontece anualmente na Faculdade Antonio
+          Meneghetti, o Code Race. No decorrer do terceiro ano de estudos, realizei estágio na área
+          de manutenção de redes de computadores e hardware. Conclui meus estudos no ano de 2016,
+          recebendo o título de Técnico em Informática.
         </p>
         <p className={styles.paragrafo}>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate temporibus dolores
-          aliquid incidunt debitis id mollitia optio praesentium tenetur ad sint porro quos,
-          placeat, corrupti obcaecati? Nostrum in illo dicta. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Aliquam dolor aliquid provident enim. Quaerat eum cum fugiat nemo a
-          libero excepturi dignissimos totam, quas sunt officia labore eaque nesciunt aliquam! Lorem
-          ipsum dolor sit amet consectetur, adipisicing elit. Provident repellat quasi mollitia ut
-          nesciunt ullam, rerum perferendis tempore quisquam voluptatum! Dolore dignissimos id sit,
-          modi rem unde mollitia at reprehenderit! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Velit sint vero ratione excepturi ducimus, quidem reiciendis corrupti
-          accusamus, vel dolorum ea. Laborum numquam obcaecati totam animi sit eos nulla fugit.
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto atque laborum,
-          voluptatem, maxime doloribus doloremque aperiam modi velit dignissimos enim aliquid quo?
-          Sed, soluta. Reiciendis id incidunt culpa reprehenderit doloribus.
+          Em 2016, passei no processo seletivo na UFSM e, durante 2 anos, cursei Ciência da
+          Computação. Após esse período, conclui que ainda não estava preparado e optei por me
+          dedicar à gastronomia. Em 2018, fui aprovado como bolsista ProUni para estudar gastronomia
+          na Unisinos Porto Alegre. Durante o período de 3 anos, através de muito estudo e prática,
+          aprimorei minhas técnicas culinárias. Participei de cursos e eventos gastronômicos.
+          Realizei estágio e trabalhei como cozinheiro freelancer. Ao fim do curso, demonstrei
+          competência nas habilidades necessárias a um profissional da área da alimentação. Foi
+          assim que, em 2021, formei-me técnico em gastronomia e retornei para minha cidade natal
+          para aplicar o conhecimento adquirido no ramo familiar.
         </p>
         <p className={styles.paragrafo}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa praesentium dolor aut, sed
-          doloribus cumque optio. Corporis, eligendi rerum quasi possimus recusandae libero fugiat
-          quod earum itaque fugit atque alias.
+          No ano de 2023, iniciei minha segunda graduação na Estácio, na área de TI, no curso de
+          Análise e Desenvolvimento de Sistemas, com o objetivo de, futuramente, me tornar um
+          desenvolvedor fullstack. Para alcançar esse objetivo, divido minha rotina entre estudos e
+          trabalho, buscando aperfeiçoar minhas habilidades
         </p>
       </h3>
     </PostModelo>
